3f21fdc559962f2dbd11d89a7ebdb1d618bbf3b5
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-03-30 22:21:23 +0530

    Create Audible Range.cpp

---
+++ Audible Range.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if (x>=67&&45000>=x){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
