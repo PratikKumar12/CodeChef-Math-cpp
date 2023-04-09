28b1ed8a5f44c137ab4f41edde2b8a6e73025c53
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-04-09 20:40:48 +0530

    Create Tasty Decisions.cpp

---
+++ Tasty Decisions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,a,b;
+	    cin>>x>>y;
+	    a=2*x;
+	    b=5*y;
+	    if(a>b)
+	    cout<<"Chocolate"<<endl;
+	    else if(a==b)
+	    cout<<"Either"<<endl;
+	    else
+	    cout<<"Candy"<<endl;
+	}
+	return 0;
+}
