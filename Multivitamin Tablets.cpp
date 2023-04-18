94a28e7bdd3e8cf6db7108e429eabce33c208b7c
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-04-18 18:48:08 +0530

    Create Multivitamin Tablets.cpp

---
+++ Multivitamin Tablets.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(b>=a*3){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
