0eedd8659165d18348c19e747a1e45af54f8f0ef
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-04-19 23:49:47 +0530

    Create Ticket Fine.cpp

---
+++ Ticket Fine.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,x;
+	    cin>>a>>b>>c;
+	    x=b-c;
+	    cout<<x*a<<endl;
+	}
+	return 0;
+}
