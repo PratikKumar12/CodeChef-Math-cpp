e556d009f1642a48bd2d68b8e5ba1acce5392195
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-04-22 18:08:21 +0530

    Create Chef and Wire Frames.cpp

---
+++ Chef and Wire Frames.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,x;
+	    cin>>a>>b>>c;
+	    x=(2*a + 2*b);
+	    cout<<x*c<<endl;
+	}
+	return 0;
+}
