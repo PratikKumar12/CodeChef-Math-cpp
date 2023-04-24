405fc5b962f728c8c58dbbd270a51dd90ab327c0
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-04-24 22:17:29 +0530

    Create Playlist.cpp

---
+++ Playlist.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b;
+	    c=(a/(b*3));
+	    cout<<c<<endl;
+	}
+	return 0;
+}
