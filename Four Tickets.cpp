a3eb33f62f521374e40c632a2f6faa74cb0b2bae
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-04-26 23:30:13 +0530

    Create Four Tickets.cpp

---
+++ Four Tickets.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    if(a*4<=1000){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
