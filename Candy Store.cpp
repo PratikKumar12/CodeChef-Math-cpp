45145ef8015408d9ef6bec18bf5e99d37377413f
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-05-04 23:22:30 +0530

    Create Candy Store.cpp

---
+++ Candy Store.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y){
+	        cout<<y<<endl;
+	    }
+	    else{
+	        cout<<x+(y-x)*2<<endl;
+	    }
+	    //z=(x+(y-x)*2);
+	    //cout<<(x+(y-x)*2)<<endl;
+	}
+	return 0;
+}
