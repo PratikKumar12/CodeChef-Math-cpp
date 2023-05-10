29319ae170a876c5ab23dea53838b4364010ef32
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-05-11 00:44:12 +0530

    Create CRED Coins.cpp

---
+++ CRED Coins.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,x;
+	    cin>>a>>b;
+	    x=a*b;
+	    cout<<x/100<<endl;
+	}
+	return 0;
+}
