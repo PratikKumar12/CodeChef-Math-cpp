24506928aa853aaa4b0b51828ad7f1e00fb5a8f8
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-05-19 01:23:39 +0530

    Create 400M Race.cpp

---
+++ 400M Race.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if(a<b && a<c){
+	        cout<<"ALICE"<<endl;
+	    }
+	    else if(b<a && b<c){
+	        cout<<"BOB"<<endl;
+	    }
+	    else{
+	        cout<<"CHARLIE"<<endl;
+	    }
+	}
+	return 0;
+}
