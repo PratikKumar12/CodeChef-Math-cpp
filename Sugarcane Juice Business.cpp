c9bcb213258c9170681865289700030fb5d4d294
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-05-21 17:08:31 +0530

    Create Sugarcane Juice Business.cpp

---
+++ Sugarcane Juice Business.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    int x=50-(50*0.70);
+	    cout<<x*a<<endl;
+	}
+	return 0;
+}
