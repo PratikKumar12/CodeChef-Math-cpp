642345de4b2a5e0fe7106343deee07e347634425
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-05-27 21:12:11 +0530

    Create Chef and Candies.cpp

---
+++ Chef and Candies.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a<b){
+	        cout<<0<<endl;
+	    }
+	    else if((a-b)%4==0){
+	        cout<<(a-b)/4<<endl;
+	    }
+	    else{
+	        cout<<(a-b)/4+1<<endl;
+	    }
+	}
+	return 0;
+}
