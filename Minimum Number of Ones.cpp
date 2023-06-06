d0939f400eeaf3967ebf018efc583635c2645684
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-06-06 08:07:47 +0530

    Create Minimum Number of Ones.cpp

---
+++ Minimum Number of Ones.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	   // for(int i=0;i<a;i++){
+	    cout<<a/2<<endl;
+	}
+	return 0;
+}
